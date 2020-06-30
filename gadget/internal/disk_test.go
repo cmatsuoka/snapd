ced2efb26d9e305d184b6b578e289ff39fae99ef
Claudio Matsuoka <claudio.matsuoka@canonical.com>
2020-06-30 14:41:28 -0300

    gadget/internal: add partition table reload test
    Signed-off-by: Claudio Matsuoka <claudio.matsuoka@canonical.com>

---
+++ gadget/internal/disk_test.go
@@ -0,0 +1,56 @@
+// -*- Mode: Go; indent-tabs-mode: t -*-
+
+/*
+ * Copyright (C) 2020 Canonical Ltd
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package internal_test
+
+import (
+	. "gopkg.in/check.v1"
+
+	"github.com/snapcore/snapd/gadget/internal"
+	"github.com/snapcore/snapd/testutil"
+)
+
+type diskSuite struct {
+	testutil.BaseTest
+}
+
+var _ = Suite(&diskSuite{})
+
+func (m *diskSuite) TestReloadPartitionTableHappy(c *C) {
+	cmdPartx := testutil.MockCommand(c, "partx", "")
+	defer cmdPartx.Restore()
+
+	err := internal.ReloadPartitionTable("/dev/node")
+	c.Assert(err, IsNil)
+	c.Assert(cmdPartx.Calls(), DeepEquals, [][]string{
+		{"partx", "-u", "/dev/node"},
+	})
+
+}
+
+func (m *diskSuite) TestReloadPartitionTableError(c *C) {
+	cmdPartx := testutil.MockCommand(c, "partx", `echo "some error"; exit 1`)
+	defer cmdPartx.Restore()
+
+	err := internal.ReloadPartitionTable("/dev/node")
+	c.Assert(err, ErrorMatches, "some error")
+	c.Assert(cmdPartx.Calls(), DeepEquals, [][]string{
+		{"partx", "-u", "/dev/node"},
+	})
+}
